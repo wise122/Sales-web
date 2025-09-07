5031f633c00f158b20661044a9a4213ac6443f9e
Aditya Bahrul Hidayat <falconblood12@gmail.com>
2025-09-07 13:57:22 +0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://dev.dahliyatrans.com", // alamat backend
-        changeOrigin: true,
-        secure: false, // kalau pakai self-signed SSL biar gak error
-      },
-    },
-  },
+  base: "/", // biar bisa di-deploy di domain root
 })
